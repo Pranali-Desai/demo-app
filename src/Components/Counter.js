268f851dcbb1005a28af276acd873eddd446006c
Pranali Desai <pranali.desai@arbitersports.com>
2023-04-17 17:25:45 +0530

    commited

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,60 +1,32 @@
 import React, { useState } from 'react';
 
- 
-
 const Counter = () => {
+const [count, setCount] = useState(0);
 
-  const [count, setCount] = useState(0);
-
- 
-
-  const handleIncrease = () => {
-
-    setCount(count => count + 1);
-
-  };
-
- 
-
-  const handleDecrease = () => {
-
-    setCount(count => count - 1);
-
-  };
-
- 
-
-  return (
-
-    <div>
-
-      <h1>Counter with useState</h1>
-
-      <p>Count: {count}</p>
-
- 
-
-      <div>
-        <button type="button" onClick={handleIncrease}>
-
-          +
-
-        </button>
-
-        <button type="button" onClick={handleDecrease}>
-
-          -
-
-        </button>
-
-      </div>
-
-    </div>
-
-  );
-
+const handleIncrease = () => {
+    setCount(count => count + 1);
 };
 
- 
+const handleDecrease = () => {
+    setCount(count => count - 1);
+};
+
+return (
+
+  <div>
+    <h1>Counter with useState</h1>
+    <p>Count: {count}</p>
+    <div>
+        <button type="button" onClick={handleIncrease}>
+            +
+        </button>
+        <button type="button" onClick={handleDecrease}>
+            -
+        </button>
+    </div>
+</div>
+);
+
+};
 
 export default Counter;
